7ade8776393da2d36eb32a1ca57992abe285e627
nurikflexc <nurikforcoding@gmail.com>
2024-06-15 08:40:31 +0500

    finally final final

--- components/Player.tsx
+++ components/Player.tsx
@@ -16,7 +16,7 @@ const Player = () => {
   }
 
   return (
-    <div className="select-none fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
       <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
